b2905d73a5d169aaba4e099b69b642ab2c014388
Yusuf EVRAN <138897280+ysfsystem@users.noreply.github.com>
2026-06-19 10:58:06 +0300

    Update versioning and download URL in recipe.sh

--- hello/recipe.sh
+++ hello/recipe.sh
@@ -1,3 +1,4 @@
-VERSION="1.0"
+[ -z "${_ver}" ] && _ver="3.0"
+VERSION="${_ver}"
 LINK_TARGET="/usr"
-DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/hello-1.0/hello-1.0.ypm"
+DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/${_pkg}-${VERSION}/${_pkg}-${VERSION}.ypm"
