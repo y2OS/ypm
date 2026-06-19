8b681ac7137dac85879f904e98aff0569ad03f10
Yusuf EVRAN <138897280+ysfsystem@users.noreply.github.com>
2026-06-19 11:13:08 +0300

    Update DOWNLOAD_URL for hello package version

--- hello/recipe.sh
+++ hello/recipe.sh
@@ -1,4 +1,4 @@
 [ -z "${_ver}" ] && _ver="3.0"
 VERSION="${_ver}"
 LINK_TARGET="/usr"
-DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/${_pkg}-${VERSION}/${_pkg}-${VERSION}.ypm"
+DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/hello-${VERSION}/hello-${VERSION}.ypm"
