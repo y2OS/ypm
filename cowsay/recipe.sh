0dfb383f534797ace95849b121652697b1925393
ysfsystem <07.yusufevran@gmail.com>
2026-06-21 20:17:11 +0300

    feat: neo-cowsay 2.0.4 eklendi

---
+++ cowsay/recipe.sh
@@ -0,0 +1,6 @@
+# Upstream Orijinal Proje: https://github.com/Code-Hex/neo-cowsay
+# License: MIT
+[ -z "${_ver}" ] && _ver="2.0.4"
+VERSION="${_ver}"
+LINK_TARGET="/" 
+DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/cowsay-${VERSION}/cowsay-${VERSION}.ypm"
