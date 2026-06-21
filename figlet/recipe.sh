748f2fa23087a54f6f5169ed83a9b621026e69b2
ysfsystem <07.yusufevran@gmail.com>
2026-06-21 19:51:58 +0300

    feat: figlet 2.2.5 eklendi

---
+++ figlet/recipe.sh
@@ -0,0 +1,6 @@
+# Upstream Orijinal Proje: https://github.com/cmatsuoka/figlet
+# License: Custom BSD-style FOSS
+[ -z "${_ver}" ] && _ver="2.2.5"
+VERSION="${_ver}"
+LINK_TARGET="/" 
+DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/figlet-${VERSION}/figlet-${VERSION}.ypm"
