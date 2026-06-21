fda7cb31170ae2678422bf7706743a09d338d2f5
ysfsystem <07.yusufevran@gmail.com>
2026-06-21 17:46:43 +0300

    feat: fastfetch 2.15.0 tarifi dogru depoya eklendi

---
+++ fastfetch/recipe.sh
@@ -0,0 +1,6 @@
+# Upstream Orijinal Proje: https://github.com/fastfetch-cli/fastfetch
+# License: MIT
+[ -z "${_ver}" ] && _ver="2.15.0"
+VERSION="${_ver}"
+LINK_TARGET="/"
+DOWNLOAD_URL="https://github.com/y2OS/y2OS/releases/download/fastfetch-${VERSION}/fastfetch-${VERSION}.ypm"
