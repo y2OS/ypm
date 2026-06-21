9d22b03f5091650b3cbb88e9a26cc6c892c2d2f3
ysfsystem <07.yusufevran@gmail.com>
2026-06-21 20:10:27 +0300

    refactor: figlet kaldırıldı

--- figlet/recipe.sh
+++
@@ -1,6 +0,0 @@
-# Upstream Orijinal Proje: https://github.com/cmatsuoka/figlet
-# License: Custom BSD-style FOSS
-[ -z "${_ver}" ] && _ver="2.2.5"
-VERSION="${_ver}"
-LINK_TARGET="/" 
-DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/figlet-${VERSION}/figlet-${VERSION}.ypm"
