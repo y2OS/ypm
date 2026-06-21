19bde0cd8293cea13ef635d5d2194df767e9f2e5
ysfsystem <07.yusufevran@gmail.com>
2026-06-21 19:45:54 +0300

    feat: sl 5.02 statik paketi ve tarifi depoya eklendi

---
+++ sl/recipe.sh
@@ -0,0 +1,7 @@
+# Upstream Orijinal Proje: https://github.com/mtoyoda/sl
+# License: Custom FOSS
+
+[ -z "${_ver}" ] && _ver="5.02"
+VERSION="${_ver}"
+LINK_TARGET="/" 
+DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/sl-${VERSION}/sl-${VERSION}.ypm"
