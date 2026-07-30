1546d498dece97807c604ab19c3ac110e28d252a
ysfsystem <07.yusufevran@gmail.com>
2026-07-30 17:40:48 +0300

    refactor: delete freesweep

--- freesweep/recipe.sh
+++
@@ -1,7 +0,0 @@
-# Upstream Orijinal Proje: https://github.com/rwestlund/freesweep
-# License: GPL-2.0-only
-
-[ -z "${_ver}" ] && _ver="1.0.2"
-VERSION="${_ver}"
-LINK_TARGET="/"
-DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/${_pkg}-${VERSION}/${_pkg}-${VERSION}.ypm"
