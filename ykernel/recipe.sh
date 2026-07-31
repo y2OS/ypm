282b1836fb29a6904323b966e0b0301d97c13037
ysfsystem <07.yusufevran@gmail.com>
2026-08-01 02:47:22 +0300

    fix: update ykernel recipe version fallback to 1.1

--- ykernel/recipe.sh
+++ ykernel/recipe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ -z "${_ver}" ] && _ver="1.0"
+[ -z "${_ver}" ] && _ver="1.1"
 VERSION="${_ver}"
 LINK_TARGET="/"
 DOWNLOAD_URL="https://github.com/y2OS/ypm/releases/download/${_pkg}-${VERSION}/${_pkg}-${VERSION}.ypm"
